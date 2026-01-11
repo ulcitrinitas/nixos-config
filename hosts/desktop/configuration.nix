5759a8bc9e695900ca8eff110ded2457d7a23762
Ulisses Silva <uli.citrinitas@gmail.com>
2026-01-10 23:50:26 -0300

    arrumado parâmetros do kernel

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -52,10 +52,10 @@
       "quiet" #
       "splash" #
       "boot.shell_on_fail" #
-      "loglevel=3"
+      # "loglevel=3"
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
-      "udev.log_priority=3" #
+      # "udev.log_priority=3" #
       "vt.global_cursor_default=0"
     ];
     # Hide the OS choice for bootloaders.
